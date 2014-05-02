f8609b2d2bed0b79d34e37c8311ebc0fd94fec1d
Mike Lam <lam@cs.umd.edu>
2014-05-02 09:26:14 -0400

    Initial work on fpinfo utility

---
+++ utils/fpinfo/fpinfo.cpp
@@ -0,0 +1,370 @@
+// standard C libs
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// STL classes
+#include <string>
+#include <vector>
+
+// DyninstAPI
+#include "BPatch.h"
+#include "BPatch_basicBlock.h"
+#include "BPatch_flowGraph.h"
+#include "BPatch_function.h"
+#include "BPatch_point.h"
+#include "BPatch_snippet.h"
+
+// PatchAPI
+#include "PatchMgr.h"
+#include "Point.h"
+#include "Snippet.h"
+
+using namespace std;
+using namespace Dyninst;
+using namespace Dyninst::InstructionAPI;
+using namespace Dyninst::PatchAPI;
+
+#define BUFFER_STRING_LEN 1024
+
+// main Dyninst driver structure
+BPatch *bpatch = NULL;
+
+// top-level Dyninst mutatee structures
+BPatch_addressSpace *mainApp = NULL;
+BPatch_image        *mainImg = NULL;
+PatchMgr::Ptr        mainMgr;
+
+BPatch_function                *printfFunc;
+BPatch_Vector<BPatch_snippet*>  initCode;
+BPatch_Vector<BPatch_snippet*>  finiCode;
+
+// analysis shared library
+BPatch_object *libObj  = NULL;
+
+// global parameters
+char *binary = NULL;
+bool instShared = false;
+
+// instructions to instrument
+const bool FILTER_ENABLED = false;
+const bool FILTER_INVERSE = false;  // false = keep ONLY instructions in filter
+                                    // true  = keep all EXCEPT named instructions
+const unsigned long FILTER_INSNS[] = { };
+const unsigned long FILTER_INSNS_SIZE = 0;
+
+BPatch_module* getInitFiniModule() {
+
+    // detects the module that should be instrumented with init/cleanup
+    BPatch_Vector<BPatch_module *> *modules = mainImg->getModules();
+    BPatch_Vector<BPatch_module *>::iterator m;
+    BPatch_Vector<BPatch_function *> initFiniFuncs;
+    bool hasInit, hasFini, isShared;
+    for (m = modules->begin(); m != modules->end(); m++) {
+        hasInit = hasFini = isShared = false;
+        if ((*m)->isSharedLib()) {
+            isShared = true;
+        }
+        initFiniFuncs.clear();
+        (*m)->findFunction("_init", initFiniFuncs, false);
+        if (initFiniFuncs.size() > 0) {
+            hasInit = true;
+        }
+        initFiniFuncs.clear();
+        (*m)->findFunction("_fini", initFiniFuncs, false);
+        if (initFiniFuncs.size() > 0) {
+            hasFini = true;
+        }
+        if (!isShared && hasInit && hasFini) {
+            return *m;
+        }
+    }
+    return NULL;
+}
+
+bool hasMutateeFunction(const char *name) {
+    BPatch_Vector<BPatch_function *> funcs;
+    mainImg->findFunction(name, funcs, true, true, true);
+    return funcs.size() > 0;
+}
+
+BPatch_function* getMutateeFunction(const char *name) {
+
+    // assumes there is a single function in the mutatee address space
+    // with the given name; finds and returns it
+    BPatch_Vector<BPatch_function *> funcs;
+    mainImg->findFunction(name, funcs, true, true, true);
+    assert(funcs.size() == 1);
+    return funcs.at(0);
+}
+
+BPatch_snippet* buildInitSnippet(BPatch_variableExpr *var) {
+    return new BPatch_arithExpr(BPatch_assign, *var, BPatch_constExpr((unsigned long)0));
+}
+
+
+BPatch_snippet* buildIncrementer(BPatch_variableExpr *var) {
+
+    // null instrumentation (for testing)
+    //BPatch_snippet *nullExpr = new BPatch_nullExpr();
+    //return PatchAPI::convert(nullExpr);
+
+    // instruction count instrumentation
+    BPatch_snippet *valExpr = new BPatch_arithExpr(BPatch_plus,
+            *var, BPatch_constExpr((unsigned long)1));
+    BPatch_snippet *incExpr = new BPatch_arithExpr(BPatch_assign,
+            *var, *valExpr);
+    return incExpr;
+}
+
+BPatch_snippet* buildFiniSnippet(void *addr, Instruction::Ptr iptr, string func, BPatch_variableExpr *var) {
+
+    // create printf call
+    stringstream disas("");
+    disas << iptr->format((Address)addr);
+    BPatch_Vector<BPatch_snippet*> printfArgs;
+    printfArgs.push_back(new BPatch_constExpr("%16lx: %-15lu %-20s %s\n"));
+    printfArgs.push_back(new BPatch_constExpr((unsigned long)addr));
+    printfArgs.push_back(var);
+    printfArgs.push_back(new BPatch_constExpr(func.c_str()));
+    printfArgs.push_back(new BPatch_constExpr(disas.str().c_str()));
+
+    return new BPatch_funcCallExpr(*printfFunc, printfArgs);
+}
+
+BPatch_snippet* buildFiniSnippet(string func, BPatch_variableExpr *var) {
+
+    // create printf call
+    BPatch_Vector<BPatch_snippet*> printfArgs;
+    printfArgs.push_back(new BPatch_constExpr("%20s: %-15d\n"));
+    printfArgs.push_back(new BPatch_constExpr(func.c_str()));
+    printfArgs.push_back(var);
+
+    return new BPatch_funcCallExpr(*printfFunc, printfArgs);
+}
+
+BPatch_snippet* buildDebugPrint(string text) {
+
+    // create printf call
+    BPatch_Vector<BPatch_snippet*> printfArgs;
+    printfArgs.push_back(new BPatch_constExpr(text.c_str()));
+    return new BPatch_funcCallExpr(*printfFunc, printfArgs);
+}
+
+void handleLibFunc(string name)
+{
+    if (!hasMutateeFunction(name.c_str())) return;
+
+    BPatch_function *func = getMutateeFunction(name.c_str());
+
+    // allocate counter
+    BPatch_variableExpr *counter = mainApp->malloc(*(mainImg->findType("unsigned long")));
+
+    // build and insert initialization
+    finiCode.push_back(buildInitSnippet(counter));
+
+    // insert instrumentation
+    BPatch_Vector<BPatch_point*> *entryPoints = func->findPoint(BPatch_entry);
+    mainApp->insertSnippet(*buildIncrementer(counter), *entryPoints);
+
+    // build and insert reporting
+    finiCode.push_back(buildFiniSnippet(name, counter));
+}
+
+
+void handleInstruction(void *addr, Instruction::Ptr iptr, PatchBlock *block, PatchFunction *func)
+{
+
+    bool handle = true;
+
+    // print instruction info
+    printf("  instruction at %lx: %s\n",
+            (unsigned long)addr, iptr->format((Address)addr).c_str());
+
+    // allocate counter
+    BPatch_variableExpr *counter = mainApp->malloc(*(mainImg->findType("unsigned long")));
+    printf("    counter at %lx\n", (unsigned long)counter->getBaseAddr());
+
+    // grab instrumentation point
+    Point *prePoint  = mainMgr->findPoint(
+                        Location::InstructionInstance(func, block, (Address)addr),
+                        Point::PreInsn, true);
+
+    // build and insert initialization
+    finiCode.push_back(buildInitSnippet(counter));
+
+    // build and insert instrumentation
+    prePoint->pushBack(PatchAPI::convert(buildIncrementer(counter)));
+
+    // build and insert reporting
+    finiCode.push_back(buildFiniSnippet(addr, iptr, func->name(), counter));
+}
+
+void handleBasicBlock(BPatch_basicBlock *block, PatchFunction *func)
+{
+    static size_t MAX_RAW_INSN_SIZE = 16;
+
+    Instruction::Ptr iptr;
+    void *addr;
+    unsigned char bytes[MAX_RAW_INSN_SIZE];
+    size_t nbytes, i;
+
+    // get all floating-point instructions
+    PatchBlock::Insns insns;
+    PatchAPI::convert(block)->getInsns(insns);
+
+    // handle each point separately
+    PatchBlock::Insns::iterator j;
+    for (j = insns.begin(); j != insns.end(); j++) {
+
+        // get instruction bytes
+        addr = (void*)((*j).first);
+        iptr = (*j).second;
+        nbytes = iptr->size();
+        assert(nbytes <= MAX_RAW_INSN_SIZE);
+        for (i=0; i<nbytes; i++) {
+            bytes[i] = iptr->rawByte(i);
+        }
+        bytes[nbytes] = '\0';
+
+        handleInstruction(addr, iptr, PatchAPI::convert(block), func);
+    }
+}
+
+void handleFunction(BPatch_function *function, const char *name)
+{
+    //printf("function %s:\n", name);
+
+    // handle all basic blocks
+    std::set<BPatch_basicBlock*> blocks;
+    std::set<BPatch_basicBlock*>::iterator b;
+    BPatch_flowGraph *cfg = function->getCFG();
+    cfg->getAllBasicBlocks(blocks);
+    for (b = blocks.begin(); b != blocks.end(); b++) {
+        handleBasicBlock(*b, PatchAPI::convert(function));
+    }
+
+    //printf("\n");
+}
+
+void handleModule(BPatch_module *mod, const char *name)
+{
+	char funcname[BUFFER_STRING_LEN];
+
+	// get list of all functions
+	std::vector<BPatch_function *>* functions;
+	functions = mod->getProcedures();
+
+	// for each function ...
+	for (unsigned i = 0; i < functions->size(); i++) {
+		BPatch_function *function = functions->at(i);
+		function->getName(funcname, BUFFER_STRING_LEN);
+
+        // CRITERIA FOR INSTRUMENTATION:
+        // don't handle:
+        //   - memset() or call_gmon_start() or frame_dummy()
+        //   - functions that begin with an underscore
+		if ( (strcmp(funcname,"memset")!=0)
+                && (strcmp(funcname,"call_gmon_start")!=0)
+                && (strcmp(funcname,"frame_dummy")!=0)
+                && funcname[0] != '_') {
+
+            handleFunction(function, funcname);
+		}
+	}
+
+}
+
+void handleApplication(BPatch_addressSpace *app)
+{
+	char modname[BUFFER_STRING_LEN];
+
+	// get a reference to the application image
+    mainApp = app;
+    mainImg = mainApp->getImage();
+    mainMgr = PatchAPI::convert(mainApp);
+
+    // get list of all modules
+    std::vector<BPatch_module *>* modules;
+    std::vector<BPatch_module *>::iterator m;
+    modules = mainImg->getModules();
+
+    printfFunc = getMutateeFunction("printf");
+
+    mainApp->beginInsertionSet();
+
+    initCode.push_back(buildDebugPrint("== Initializing fpinfo analysis ==\n"));
+
+    // for each module ...
+    for (m = modules->begin(); m != modules->end(); m++) {
+        (*m)->getName(modname, BUFFER_STRING_LEN);
+
+        // for the purposes of this test,
+        // don't handle our own library or libm
+        if (strcmp(modname, "libm.so.6") == 0 ||
+            strcmp(modname, "libc.so.6") == 0) {
+            //printf(" skipping %s [%s]\n", name, modname);
+            continue;
+        }
+
+        // don't handle shared libs unless requested
+        if ((*m)->isSharedLib() && !instShared) {
+            continue;
+        }
+
+        handleModule(*m, modname);
+    }
+
+    handleLibFunc("sin");
+    handleLibFunc("cos");
+    handleLibFunc("sqrt");
+    handleLibFunc("f_approx_equal");
+    handleLibFunc("d_approx_equal");
+
+    finiCode.push_back(buildDebugPrint("== Done with fpinfo analysis ==\n"));
+
+    // add initialization/cleanup at init/fini
+    BPatch_module *initFiniModule = getInitFiniModule();
+    assert(initFiniModule != NULL);
+    BPatch_sequence *initCall = new BPatch_sequence(initCode);
+    BPatch_sequence *finiCall = new BPatch_sequence(finiCode);
+    initFiniModule->insertInitCallback(*initCall);
+    initFiniModule->insertFiniCallback(*finiCall);
+
+    mainApp->finalizeInsertionSet(false); 
+}
+
+int main(int argc, char *argv[])
+{
+    // ABBREVIATED: parse command-line parameters
+    binary = argv[1];
+    instShared = false;
+
+	// initalize DynInst library
+	bpatch = new BPatch;
+
+    printf("Testing %s\n", binary);
+
+    // open binary (and possibly dependencies)
+	BPatch_addressSpace *app;
+    if (instShared) {
+        app = bpatch->openBinary(binary, true);
+    } else {
+        app = bpatch->openBinary(binary, false);
+    }
+	if (app == NULL) {
+		printf("ERROR: Unable to open application.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    app->loadLibrary("libc.so.6");
+
+    // perform instrumentation
+    handleApplication(app);
+
+    ((BPatch_binaryEdit*)app)->writeFile("mutant");
+    printf("Done.\n");
+
+	return(EXIT_SUCCESS);
+}
+
